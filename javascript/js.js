1a039a6e7883fc5b75ef8c7747d99e1697e898ed
JsRashkow <35548230+JsRashkow@users.noreply.github.com>
2018-11-28 19:21:20 -0600

    add mobile styles

--- javascript/js.js
+++ javascript/js.js
@@ -1,28 +1,7 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function send() {
+  alert("Thank You for Registering! An email with more information will be sent out shortly!");
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+function sendcon() {
+  alert("Thank You for Contacting us! A personalized reply will be sent out within 5-7 buisness days.");
 }
